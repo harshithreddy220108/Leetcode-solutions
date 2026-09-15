f8aca870b57be9f8dac3770fd033bbc95490bfb0
Harshith Reddy <harshith220108@gmail.com>
2026-09-15 13:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (16.37%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        f=0
+        t=0
+        for b in bills:
+            if b==5:
+                f+=1
+            elif b==10:
+                if f==0:
+                    return False
+                f-=1
+                t+=1
+            elif b==20:
+                if t>0 and f>0:
+                    t-=1
+                    f-=1
+                elif f>=3:
+                    f-=3
+                else:
+                    return False
+        return True
